d3aa516a2b6791a08ed2b5e743033301dae623e1
Wanfeng.Shamoo <wanfeng.shamoo@pacificlife.com>
2020-12-30 15:35:14 -0800

    add a note

--- orbit-app/src/util/fetch.js
+++ orbit-app/src/util/fetch.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/** For endpoints that do not require authentications.
+ * such as, /api/signup, /api/authenticate
+ */
 const publicFetch = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 });
