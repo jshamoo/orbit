73d63dad53d6ed8be11f23255785acf06f6c9b1d
Wanfeng.Shamoo <wanfeng.shamoo@pacificlife.com>
2021-01-01 13:00:28 -0800

    attach user to req

--- orbit-api/server.js
+++ orbit-api/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwtDecode = require('jwt-decode');
 const mongoose = require('mongoose');
+const jwt = require('express-jwt')
 
 const dashboardData = require('./data/dashboard');
 const User = require('./data/User');
@@ -134,9 +135,34 @@ app.post('/api/signup', async (req, res) => {
   }
 });
 
-app.get('/api/dashboard-data', (req, res) =>
-  res.json(dashboardData)
-);
+const attachUser = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({message: 'Authentication invalid'})
+  }
+
+  const decodedToken = jwtDecode(token.slice(7));
+  if (!decodedToken) {
+    return res.status(401).json({message: 'There was a problem authorizing the request'})
+  } else {
+    req.user = decodedToken;
+    next();
+  }
+}
+
+app.use(attachUser)
+
+const checkJwt = jwt({
+  secret: process.env.JWT_SECRET,
+  issuer: 'api.orbit',
+  audience: 'api.orbit'
+})
+
+
+app.get('/api/dashboard-data', checkJwt, (req, res) => {
+  console.log(req.user)
+  return res.json(dashboardData)
+});
 
 app.patch('/api/user-role', async (req, res) => {
   try {
