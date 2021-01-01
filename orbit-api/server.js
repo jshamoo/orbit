a0e2b9212819b1f9355a6fa788d1812174b935d6
Wanfeng.Shamoo <wanfeng.shamoo@pacificlife.com>
2021-01-01 13:09:08 -0800

    protect inventory endpoint

--- orbit-api/server.js
+++ orbit-api/server.js
@@ -158,6 +158,14 @@ const checkJwt = jwt({
   audience: 'api.orbit'
 })
 
+const requireAdmin = (req, res, next) => {
+  const {role} = req.user;
+  if (role != 'admin') {
+    return res.status(401).json({message: 'Insufficient role'})
+  }
+  next();
+}
+
 
 app.get('/api/dashboard-data', checkJwt, (req, res) => {
   console.log(req.user)
@@ -187,7 +195,7 @@ app.patch('/api/user-role', async (req, res) => {
   }
 });
 
-app.get('/api/inventory', async (req, res) => {
+app.get('/api/inventory', checkJwt, requireAdmin, async (req, res) => {
   try {
     const inventoryItems = await InventoryItem.find();
     res.json(inventoryItems);
@@ -196,7 +204,7 @@ app.get('/api/inventory', async (req, res) => {
   }
 });
 
-app.post('/api/inventory', async (req, res) => {
+app.post('/api/inventory', checkJwt, requireAdmin, async (req, res) => {
   try {
     const inventoryItem = new InventoryItem(req.body);
     await inventoryItem.save();
@@ -212,7 +220,7 @@ app.post('/api/inventory', async (req, res) => {
   }
 });
 
-app.delete('/api/inventory/:id', async (req, res) => {
+app.delete('/api/inventory/:id', checkJwt, requireAdmin, async (req, res) => {
   try {
     const deletedItem = await InventoryItem.findOneAndDelete(
       { _id: req.params.id }
