c788706d5779d56a0dd8186fbb9e175a361403a7
Wanfeng.Shamoo <wanfeng.shamoo@pacificlife.com>
2021-01-01 11:37:33 -0800

    Save auth state in local storage

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -4,18 +4,26 @@ const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  const token = localStorage.getItem('token')
+  const userInfo = localStorage.getItem('userInfo')
+  const expiresAt = localStorage.getItem('expiresAt')
+
   const [authState, setAuthState] = useState({
-    token: null,
-    expiresAt: null,
-    userInfo: {}
+    token,
+    expiresAt,
+    userInfo: userInfo ? JSON.parse(userInfo) : {}
   });
   
   const setAuthInfo = ({token, expiresAt, userInfo}) => {
+    localStorage.setItem('token', token)
+    localStorage.setItem('userInfo', JSON.stringify(userInfo))
+    localStorage.setItem('expiresAt', expiresAt)
+
     setAuthState({
       token,
       expiresAt,
       userInfo
-    })
+    });
   }
 
   return (
